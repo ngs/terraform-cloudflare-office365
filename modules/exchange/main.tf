b85dc72cac9737b099753b1822f009cb83c4bf76
Atsushi NAGASE <a@ngs.io>
2026-02-12 13:09:40 +0900

    chore: Update Cloudflare provider to ~5.0
    Upgrade Cloudflare Terraform provider version constraint from ~3.0 to
    ~5.0 across all modules (exchange, mdm, skype, verification), root
    module, and test configuration.
    
    Also update GitHub Actions workflow to use hashicorp/setup-terraform@v3
    and actions/checkout@v6.

--- modules/exchange/main.tf
+++ modules/exchange/main.tf
@@ -1,24 +1,24 @@
-resource "cloudflare_record" "mx" {
-  zone_id   = var.zone_id
-  name      = var.name
-  value     = var.mx
-  type      = "MX"
-  priority  = 0
-  ttl       = 3600
+resource "cloudflare_dns_record" "mx" {
+  zone_id  = var.zone_id
+  name     = var.name
+  content  = var.mx
+  type     = "MX"
+  priority = 0
+  ttl      = 3600
 }
 
-resource "cloudflare_record" "spf" {
-  zone_id   = var.zone_id
-  name      = var.name
-  value     = "v=spf1 include:spf.protection.outlook.com -all"
-  type      = "TXT"
-  ttl       = 3600
+resource "cloudflare_dns_record" "spf" {
+  zone_id = var.zone_id
+  name    = var.name
+  content = "v=spf1 include:spf.protection.outlook.com -all"
+  type    = "TXT"
+  ttl     = 3600
 }
 
-resource "cloudflare_record" "autodiscover" {
-  zone_id   = var.zone_id
-  name      = "autodiscover${var.name == "@" ? "" : ".${var.name}"}"
-  value     = "autodiscover.outlook.com"
-  type      = "CNAME"
-  ttl       = 3600
+resource "cloudflare_dns_record" "autodiscover" {
+  zone_id = var.zone_id
+  name    = "autodiscover${var.name == "@" ? "" : ".${var.name}"}"
+  content = "autodiscover.outlook.com"
+  type    = "CNAME"
+  ttl     = 3600
 }
--- modules/skype/main.tf
+++ modules/skype/main.tf
@@ -1,28 +1,26 @@
-resource "cloudflare_record" "sipdir-online" {
-  zone_id  = var.zone_id
-  name     = "sip${var.name == "@" ? "" : ".${var.name}"}"
-  value    = "sipdir.online.lync.com"
-  type     = "CNAME"
-  ttl      = 3600
+resource "cloudflare_dns_record" "sipdir-online" {
+  zone_id = var.zone_id
+  name    = "sip${var.name == "@" ? "" : ".${var.name}"}"
+  content = "sipdir.online.lync.com"
+  type    = "CNAME"
+  ttl     = 3600
 }
 
-resource "cloudflare_record" "webdir-online" {
-  zone_id  = var.zone_id
-  name     = "lyncdiscover${var.name == "@" ? "" : ".${var.name}"}"
-  value    = "webdir.online.lync.com"
-  type     = "CNAME"
-  ttl      = 3600
+resource "cloudflare_dns_record" "webdir-online" {
+  zone_id = var.zone_id
+  name    = "lyncdiscover${var.name == "@" ? "" : ".${var.name}"}"
+  content = "webdir.online.lync.com"
+  type    = "CNAME"
+  ttl     = 3600
 }
 
-resource "cloudflare_record" "sip-tls" {
-  zone_id  = var.zone_id
-  name     = var.name
-  type     = "SRV"
+resource "cloudflare_dns_record" "sip-tls" {
+  zone_id = var.zone_id
+  name    = "_sip._tls.${var.name == "@" ? "" : "${var.name}."}"
+  type    = "SRV"
+  ttl     = 3600
 
-  data {
-    service  = "_sip"
-    proto    = "_tls"
-    name     = var.name
+  data = {
     priority = 100
     weight   = 1
     port     = 443
@@ -30,15 +28,13 @@ resource "cloudflare_record" "sip-tls" {
   }
 }
 
-resource "cloudflare_record" "sipfederationtls-tcp" {
-  zone_id  = var.zone_id
-  name     = var.name
-  type     = "SRV"
+resource "cloudflare_dns_record" "sipfederationtls-tcp" {
+  zone_id = var.zone_id
+  name    = "_sipfederationtls._tcp.${var.name == "@" ? "" : "${var.name}."}"
+  type    = "SRV"
+  ttl     = 3600
 
-  data {
-    service  = "_sipfederationtls"
-    proto    = "_tcp"
-    name     = var.name
+  data = {
     priority = 100
     weight   = 1
     port     = 5061
